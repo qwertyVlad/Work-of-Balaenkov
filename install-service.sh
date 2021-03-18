b32879fba6841d4a64de8a6dd70d275f839701f1
qwertyVlad <67752113+qwertyVlad@users.noreply.github.com>
2021-03-18 22:38:43 +0300

    Update install-service.sh

--- install-service.sh
+++ install-service.sh
@@ -8,7 +8,7 @@ echo /dev/null > "$LOG_PATH"
 while (true)
   do
      current_date_time="`date "+%Y-%m-%d %H:%M:%S"`"
-      echo "[$current_date_time]: im allive" >> "$LOG_PATH"
+      echo "[$current_date_time]: Vlad is working" >> "$LOG_PATH"
       sleep 10;
       echo "10 second"
   done;
